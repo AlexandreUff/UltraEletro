43cd4bb67f42661c6e5071b08a49e352799136a3
AlexandreUff <97979545+AlexandreUff@users.noreply.github.com>
2022-03-08 14:12:34 -0300

    6º Commit: Alteração dos dados pela query
    Alteração do local da função que transforma o preço em parcelas e alteração no envio dos dados à página de compra.

--- src/components/items/ProductMain.jsx
+++ src/components/items/ProductMain.jsx
@@ -4,47 +4,45 @@ import Link from 'next/link'
 
 import styles from './ProductMain.module.css'
 
-function freteTipo(tipo){
-    if(tipo){
+function freteTipo(tipo) {
+    if (tipo) {
         return <p className={styles.freteGratis}>
-                    <Ray/> <strong>Frente Grátis!</strong>
-            </p>
+            <Ray /> <strong>Frente Grátis!</strong>
+        </p>
     } else {
         return <p className={styles.fretePago}>
-                    Frete a partir de R$ 12,50
-            </p>
+            Frete a partir de R$ 12,50
+        </p>
     }
 }
 
-let precoParcela;
 
-function parcela(preco){
-
-    /* Pega o preco e o converte em parcelas */
-    const parc = preco / 10;
-    const int = parc.toString().split('.')[0];
-    const frac = parc.toString().split('.')[1].slice(0,2);
-
-    precoParcela = `${int},${frac}`;
-    
-    return <>{int} <sup style={{marginLeft:'5px'}}>{frac}</sup></>
-}
-
-export default function ProductMain(props){
+export default function ProductMain(props) {
     const precoRefeito = props.preco.toString().replace('.', ',');
-    /* const precoParcela */
+
+    function parcela(preco) {
+
+        /* Pega o preco e o converte em parcelas */
+        const parc = preco / 10;
+        const int = parc.toString().split('.')[0];
+        const frac = parc.toString().split('.')[1].slice(0, 2);
+
+        return [int,frac];
+    }
+
+    const precoParcela = parcela(props.preco);
 
     /* O link abaixo envia, pela barra de endereço, os dados do produto clicado e, com base nesses dados, renderiza a página.*/
-    return <Link href={`${window.location}/produtos?nome=${props.nome}&thumb=${props.thumb}&preco=${precoRefeito}&parc=${precoParcela}${props.frete ? `&frete=${props.frete}` : ''}&estrelas=${props.estrelas}&desc=${props.desc}`} passHref> 
+    return <Link href={`${window.location}/produtos?nome=${props.nome}&thumb=${props.thumb}&preco=${precoRefeito}&parc=${precoParcela[0]},${precoParcela[1]}${props.frete ? `&frete=${props.frete}` : ''}&estrelas=${props.estrelas}&desc=${props.desc}`} passHref>
         <div className={styles.content} title={props.nome}>
-                {/* A mensagem de bem avaliado só é exibida se for avaliação máxima*/}
-                {props.estrelas === 5 ? <div className={styles.avaliado}>★ BEM AVALIADO</div> : ''}
+            {/* A mensagem de bem avaliado só é exibida se for avaliação máxima*/}
+            {props.estrelas === 5 ? <div className={styles.avaliado}>★ BEM AVALIADO</div> : ''}
 
             {/* <img src={`/design/imgs-produtos/${props.thumb}.jpg`} alt="Imagem do produto " /> */}
             <Image src={`/design/imgs-produtos/${props.thumb}.jpg`} alt="Imagem do produto " width={200} height={140} />
             <h3>{props.nome}</h3>
             <h4>R$ {precoRefeito}</h4>
-            <p>Em 10x de {parcela(props.preco)}</p>
+            <p>Em 10x de {precoParcela[0]} <sup style={{ marginLeft: '5px' }}>{precoParcela[1]}</sup></p>
 
             <hr />
 
